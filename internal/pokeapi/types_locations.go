9e4db1a1238c265da4c951731e552d79d6d14a7d
Young <yongchihua@gmail.com>
2023-10-04 21:03:01 +0800

    Implement Caching

--- internal/pokeapi/types_locations.go
+++ internal/pokeapi/types_locations.go
@@ -1,6 +1,7 @@
 package pokeapi
 
 // RespShallowLocations -
+// 希望字段能够被 JSON 编码和解码，以及在不同包中被访问，需要将它的首字母改为大写
 type RespShallowLocations struct {
 	Count    int     `json:"count"`
 	Next     *string `json:"next"`
